e5c6fde0aafe451c8ac679943a64acc73a462ff9
mepmepp <melissa.emorine@efrei.net>
2025-10-14 13:49:43 +0200

    exercice 6

--- express_server.js
+++ express_server.js
@@ -1,11 +1,15 @@
-const express = require('express')
-const app = express()
-const port = 3000
+const express = require('express');
+const app = express();
+const port = 3000;
 
 app.get('/', (req, res) => {
-  res.send('Hello World!')
-})
+  res.send('Hello World!');
+});
+
+app.get('/some-html', (req, res) => {
+  res.send('<html><body><h1>Bonjour html !</h1></body></html>');
+});
 
 app.listen(port, () => {
-  console.log(`Example app listening on port ${port}`)
-})
+  console.log(`Example app listening on port ${port}`);
+});
