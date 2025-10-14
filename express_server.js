1998ae681230e51d49a7dab0da021856f0350727
mepmepp <melissa.emorine@efrei.net>
2025-10-14 14:01:03 +0200

    exercice 7

--- express_server.js
+++ express_server.js
@@ -10,6 +10,14 @@ app.get('/some-html', (req, res) => {
   res.send('<html><body><h1>Bonjour html !</h1></body></html>');
 });
 
+app.get('/some-json', (req, res) => {
+  const someJson = {
+    'age': 22,
+    'nom': 'Jane'
+  };
+  res.json(someJson);
+});
+
 app.listen(port, () => {
   console.log(`Example app listening on port ${port}`);
 });
